3ca1d8274aa9a46c52dbc81d6834ed091ad2b5f2
ystephant <134706137+ystephant@users.noreply.github.com>
2026-02-06 00:37:16 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
-      <Head />
+    <Html lang="fr">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#9333ea" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Inventaire Jeux" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
