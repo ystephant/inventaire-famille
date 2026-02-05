a325de7ee27b0aae73a31571845ccfe4444e0ded
ystephant <134706137+ystephant@users.noreply.github.com>
2026-02-06 00:39:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Exclure le service worker du build
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
